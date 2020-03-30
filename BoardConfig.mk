168d347fd38599fee358c04d21aba525202ea468
sjllls <2012ji123@gmail.com>
2020-03-30 21:45:08 +0800

    BoardConfig.mk: Update defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/sony/akari
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := akari_defconfig
+TARGET_KERNEL_CONFIG := tama_akari_defconfig
 
 # Selinux
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
